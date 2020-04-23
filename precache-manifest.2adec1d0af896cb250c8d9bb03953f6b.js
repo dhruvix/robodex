7c09f6d57330801e5f00b1f6031a11177625ed54
dhruvix <dhruvix7@gmail.com>
2020-04-23 21:55:58 +0530

    Updates

---
+++ precache-manifest.2adec1d0af896cb250c8d9bb03953f6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0f2ffeb037d2e26448c66c48b9921a",
+    "url": "/robodex/index.html"
+  },
+  {
+    "revision": "97082e67f799ca5dbcd2",
+    "url": "/robodex/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "07baf10a0b111d751f7f",
+    "url": "/robodex/static/css/main.ba6d2461.chunk.css"
+  },
+  {
+    "revision": "97082e67f799ca5dbcd2",
+    "url": "/robodex/static/js/2.5ffd4c61.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robodex/static/js/2.5ffd4c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07baf10a0b111d751f7f",
+    "url": "/robodex/static/js/main.67e4cd08.chunk.js"
+  },
+  {
+    "revision": "e6712b980b42e81d21c6",
+    "url": "/robodex/static/js/runtime-main.a53e1754.js"
+  }
+]);
